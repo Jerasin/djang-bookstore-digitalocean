7e4ec9db493dfbd03fd370f204f5d1356d9f1495
Jerasin <choi.jerasin@gmail.com>
2021-09-11 00:28:16 +0700

    DEBUG LOG

--- bookstore/settings/production.py
+++ bookstore/settings/production.py
@@ -34,7 +34,7 @@ LOGGING = {
         "file": {
             "level": "INFO",
             "class": "logging.FileHandler",
-            "filename": "/logs/bookstore.log",
+            "filename": '/bookstore/logs/bookstore.log',
             "formatter": "app",
         },
     },
